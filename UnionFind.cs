be83e5ff003fccfd9e14c8c51c259a9242028d76
tomasz-herman <tomasz.herman@protonmail.com>
2019-08-22 04:07:19 +0200

    UnionFind docs

--- UnionFind.cs
+++ UnionFind.cs
@@ -7,7 +7,8 @@ namespace ASD.Graphs
     /// </summary>
     /// <remarks>
     /// W C# to oczywiście klasa, a nie struktura.<para/>
-    /// To nie jest optymalna wydajnościowo implementacja (ale calkiem dobra !). Implementuje kompresję ścieżek, nie implementuje łączenia według rang.
+    /// To nie jest optymalna wydajnościowo implementacja (ale calkiem dobra !). Implementuje kompresję ścieżek,
+    /// nie implementuje łączenia według rang.
     /// Implementacja ta jest za to zdecydowanie najprostsza.<para/>
     /// Wymaga aby badane elementy były identyfikowane za pomocą liczb całkowitych 0,1,...,n-1 (dla problemu rozmiaru n).
     /// </remarks>
@@ -17,15 +18,30 @@ namespace ASD.Graphs
     {
         private readonly int[] _gr;
 
-        public UnionFind(int num)
+        /// <summary>
+        /// Inicjuje dane
+        /// </summary>
+        /// <param name="n">Rozmiar problemu</param>
+        /// <remarks>Każdy element jest swoim własnym reprezentantem (jest w oddzielnym zbiorze).</remarks>
+        /// <seealso cref="UnionFind"/>
+        /// <seealso cref="ASD.Graphs"/>
+        public UnionFind(int n)
         {
-            _gr = new int[num];
-            for (var i = 0; i < num; i++)
+            _gr = new int[n];
+            for (var i = 0; i < n; i++)
             {
                 _gr[i] = i;
             }
         }
 
+        /// <summary>
+        /// Łączy dwa zbiory
+        /// </summary>
+        /// <param name="n1">Element pierwszego zbioru</param>
+        /// <param name="n2">Element drugiego zbioru</param>
+        /// <returns>Zwraca true jeśli rzeczywiście dokonano połączenia (parametry reprezentowały rózne zbiory)</returns>
+        /// <seealso cref="UnionFind"/>
+        /// <seealso cref="ASD.Graphs"/>
         public bool Union(int n1, int n2)
         {
             n1 = Find(n1);
@@ -38,6 +54,14 @@ namespace ASD.Graphs
             return true;
         }
 
+        /// <summary>
+        /// Znajduje reprezentanta zbioru (elementu)
+        /// </summary>
+        /// <param name="n">Dowolny element</param>
+        /// <returns>Reprezentant danego elementu</returns>
+        /// <remarks>Wykonuje kompresję ścieżki.</remarks>
+        /// <seealso cref="UnionFind"/>
+        /// <seealso cref="ASD.Graphs"/>
         public int Find(int n)
         {
             if (_gr[_gr[n]] != _gr[n])
