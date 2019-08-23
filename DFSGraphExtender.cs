1bcd48d3c06eccddfbf0c7b03ae81e1a5a8e95a0
tomasz-herman <tomasz.herman@protonmail.com>
2019-08-23 05:13:04 +0200

    DFS docs

--- DFSGraphExtender.cs
+++ DFSGraphExtender.cs
@@ -9,6 +9,47 @@ namespace ASD.Graphs
     /// <seealso cref="ASD.Graphs"/>
     public static class DFSGraphExtender
     {
+        /// <summary>
+        /// Przeszukuje cały graf w głąb (rekurencyjne)
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="preVisit">Metoda wywoływana przy pierwszym wejściu do wierzchołka</param>
+        /// <param name="postVisit">Metoda wywoływana przy ostatecznym opuszczaniu wierzchołka</param>
+        /// <param name="cc">Liczba "spójnych składowych" grafu (parametr wyjściowy)</param>
+        /// <param name="nr">Tablica kolejności "wierzchołków startowych"</param>
+        /// <returns>Informacja czy zbadano wszystkie wierzchołki grafu</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <remarks>
+        /// Metoda odwiedza wszystkie wierzchołki grafu wykorzystując metodę
+        /// <see cref="DFSearchFrom"/> (w razie potrzeby wywołuje ją wielokrotnie).<para/>
+        /// Jako wierzchołek startowy dla metody <see cref="DFSearchFrom"/> wybierany jest
+        /// pierwszy nieodwiedzony wierzchołek nr[i], gdzie tablica nr przeglądana
+        /// jest w kierunku rosnących indeksów.<para/>
+        /// Domyślna wartość parametru nr (null) oznacza, że tablica nr zostanie
+        /// automatycznie wypełniona wartościami nr[i]=i (kolejność domyślna).<para/>
+        /// Wartością parametru wyjściowego cc jest wykonana liczba wywołań metody <see cref="DFSearchFrom"/>
+        /// (dla grafów nieskierowanych jest to liczba spójnych składowych grafu,
+        /// dla grafów skierowanych oczywiście nie ma takiej zależności).<para/>
+        /// Wartość parametru cc jest inkrementowana przed każdym wywołaniem metody <see cref="DFSearchFrom"/>.<para/>
+        /// Metoda oczywiście odwiedza wierzchołki jednokrotnie, zwrot "ostateczne opuszczenie wierzchołka"
+        /// oznacza moment, w którym mechamizm rekursji opuszcza wierzchołek.<para/>
+        /// Parametrem metod preVisit i postVisit jest numer odwiedzanego wierzchołka.
+        /// Wyniki zwracane przez metody preVisit i postVisit oznaczają
+        /// żądanie kontynuowania (true) lub przerwania (false) obliczeń.<para/>
+        /// Parametry preVisit i postVisit mogą mieć wartość null,
+        /// co oznacza metodę pustą (nie wykonującą żadnych działań, zwracającą true).<para/>
+        /// Jeśli zostały zbadane wszystkie wierzchołki grafu metoda zwraca true,
+        /// jeśli obliczenia zostały przerwane przez metody związane
+        /// z parametrami preVisit lub postVisit metoda zwraca false.<para/>
+        /// Uwaga:<para/>
+        /// Dla dużych grafów metoda może spowodować przepełnienie stosu
+        /// (z powodu zbyt dużej liczby zagnieżdżonych wywołań rekurencyjnych).<para/>
+        /// W takim przypadku można zastosować metodę
+        /// <see cref="GeneralSearchGraphExtender.GeneralSearchAll{TEdgesContainer}"/>
+        /// z wykorzystaniem kontenera <see cref="EdgesStack"/> (jawnego stosu).
+        /// </remarks>
+        /// <seealso cref="DFSGraphExtender"/>
+        /// <seealso cref="ASD.Graphs"/>
         public static bool DFSearchAll(this Graph g, Predicate<int> preVisit, Predicate<int> postVisit, out int cc, int[] nr = null)
         {
             if (nr != null)
@@ -41,6 +82,44 @@ namespace ASD.Graphs
             return true;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="g">Badany graf</param>
+        /// <param name="from">Wierzchołek startowy</param>
+        /// <param name="preVisit">Metoda wywoływana przy pierwszym wejściu do wierzchołka</param>
+        /// <param name="postVisit">Metoda wywoływana przy ostatecznym opuszczaniu wierzchołka</param>
+        /// <param name="visitedVertices">Tablica odwiedzonych wierzchołków</param>
+        /// <returns>Informacja czy zbadano wszystkie wierzchołki osiągalne z wierzchołka startowego</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <remarks>
+        /// Metoda odwiedza jedynie te wierzchołki, dla których elementy tablicy visitedVertices mają wartość false
+        /// (i zmienia wpisy w visitedVertices na true).<para/>
+        /// Tablica visitedVertices musi mieć rozmiar zgodny z liczbą wierzchołków grafu g.<para/>
+        /// Domyślna wartość null parametru visitedVertices oznacza tablicę wypełnioną
+        /// wartościami false (zostanie ona utworzona wewnątrz metody).<para/>
+        /// Metoda oczywiście odwiedza wierzchołki jednokrotnie, zwrot
+        /// "ostateczne opuszczenie wierzchołka" oznacza moment,
+        /// w którym mechamizm rekursji opuszcza wierzchołek.<para/>
+        /// Parametrem metod preVisit i postVisit jest numer odwiedzanego wierzchołka.<para/>
+        /// Wyniki zwracane przez metody preVisit i postVisit oznaczają
+        /// żądanie kontynuowania (true) lub przerwania (false) obliczeń.<para/>
+        /// Parametry preVisit i postVisit mogą mieć wartość null, co oznacza metodę pustą
+        /// (nie wykonującą żadnych działań, zwracającą true).<para/>
+        /// Jeśli zostały zbadane wszystkie wierzchołki osiągalne z wierzchołka startowego metoda zwraca true,
+        /// jeśli obliczenia zostały przerwane przez metody związane z parametrami preVisit lub postVisit
+        /// metoda zwraca false.<para/>
+        /// Wynik true nie oznacza, że metoda odwiedziła wszystkie wierzchołki grafu
+        /// (oznacza jedynie, że odwiedziła wszystkie wierzchołki osiągalne z wierzchołka startowego).<para/>
+        /// Uwaga:<para/>
+        /// Dla dużych grafów metoda może spowodować przepełnienie stosu
+        /// (z powodu zbyt dużej liczby zagnieżdżonych wywołań rekurencyjnych).<para/>
+        /// W takim przypadku można zastosować metodę
+        /// <see cref="GeneralSearchGraphExtender.GeneralSearchAll{TEdgesContainer}"/> z wykorzystaniem
+        /// kontenera <see cref="EdgesStack"/> (jawnego stosu).<para/>
+        /// </remarks>
+        /// <seealso cref="DFSGraphExtender"/>
+        /// <seealso cref="ASD.Graphs"/>
         public static bool DFSearchFrom(this Graph g, int from, Predicate<int> preVisit, Predicate<int> postVisit, bool[] visitedVertices = null)
         {
             if (visitedVertices == null)
